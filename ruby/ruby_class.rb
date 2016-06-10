cbeeb2b7fd2460ea756cc5dfc1eee660ffcefca4
rameshgunner <bykalpt@gmail.com>
2016-06-10 07:23:18 +0545

    Added Inheritance

--- ruby/ruby_class.rb
+++ ruby/ruby_class.rb
@@ -1,19 +1,23 @@
 class Item
-	def initialize(item_name, quantity)
-		@item_name = item_name
-		@quantity = quantity
-
-
-	end
-
-	private
-	def show
-		puts @item_name
-		puts @quantity
-		supplier = "Ramesh Inc"
-		puts supplier
-	end
+  def initialize(item_name, quantity)
+    @item_name = item_name
+    @quantity = quantity
+  end
+  
+  def quantity=(new_quantity)
+  	@quantity = new_quantity
+      # your code here
+  end
+  
+  def quantity
+  @quantity
+  end 
 end
 
-Item.new("Sony",1).show
-Item.new("Samsung",2).show
+
+Item.new("a",1).quantity
+item = Item.new("a",2)
+item = Item.new("a",2)
+item = Item.new("a",2)
+item.quantity =3
+p item.quantity
